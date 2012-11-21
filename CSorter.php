c6c808f89414c020473dacfb760e07a6c3ad550f
OpenShift guest <benjamin.woester@gmail.com>
2012-11-21 18:05:52 -0500

    basic sorter factory, knowing wrappers for php builtin sort methods

--- CSorter.php
+++ CSorter.php
@@ -2,13 +2,14 @@
 
 abstract class CSorter implements ISorter
 {
-    const SORT_BY_VALUE = 0;    // default if not specified
-    const SORT_BY_KEY   = 1;
+    const SORT_BY_VALUE     = 0;    // default if not specified
+    const SORT_BY_KEY       = 1;
+    const SORT_BY_CALLBACK  = 2;
     
-    const MAINTAIN_KEY_ASSOCIATION = 2;
+    const MAINTAIN_KEY_ASSOCIATION = 4;
     
     const ORDER_ASC     = 0;    // default if not specified
-    const ORDER_DESC    = 4;
+    const ORDER_DESC    = 8;
     
-    const REQUIRE_STABLE    = 8;
+    const REQUIRE_STABLE    = 16;
 }
