ff5ee65bc0971e2ee6bb89a1869f36ea69d6d616
OpenShift guest <benjamin.woester@gmail.com>
2012-11-22 17:28:35 -0500

    define alias constants

--- CSorter.php
+++ CSorter.php
@@ -3,8 +3,9 @@
 abstract class CSorter implements ISorter
 {
     const SORT_BY_VALUE     = 0;    // default if not specified
-    const SORT_BY_KEY       = 1;
-    const SORT_BY_CALLBACK  = 2;
+    const SORT_BY_KEY       = 1 | self::MAINTAIN_KEY_ASSOCIATION;
+    
+    const COMPARE_BY_CALLBACK  = 2;
     
     const MAINTAIN_KEY_ASSOCIATION = 4;
     
@@ -12,4 +13,16 @@ abstract class CSorter implements ISorter
     const ORDER_DESC    = 8;
     
     const REQUIRE_STABLE    = 16;
+    
+    const ASORT       = self::SORT_BY_VALUE | self::MAINTAIN_KEY_ASSOCIATION;
+    const ARSORT      = self::SORT_BY_VALUE | self::MAINTAIN_KEY_ASSOCIATION | self::ORDER_DESC;
+    const KRSORT      = self::SORT_BY_KEY | self::ORDER_DESC;
+    const KSORT       = self::SORT_BY_KEY;
+    // const NATCASESORT = self::SORT_BY_VALUE | self::MAINTAIN_KEY_ASSOCIATION;
+    // const NATSORT     = self::SORT_BY_VALUE | self::MAINTAIN_KEY_ASSOCIATION;
+    const RSORT       = self::SORT_BY_VALUE | self::ORDER_DESC;
+    const SORT        = self::SORT_BY_VALUE;
+    const UASORT      = self::SORT_BY_VALUE | self::COMPARE_BY_CALLBACK | self::MAINTAIN_KEY_ASSOCIATION;
+    const UKSORT      = self::SORT_BY_KEY | self::COMPARE_BY_CALLBACK;
+    const USORT       = self::SORT_BY_VALUE | self::COMPARE_BY_CALLBACK;
 }
